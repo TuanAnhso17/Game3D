b8f348a58b9ca2ebf9062cd47fec1bb5bf4d689f
HoangLam712005 <zzlam123@gmail.com>
2025-02-23 18:39:13 +0700

    MainUI

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1); // Tải scene có index 1, đảm bảo bạn đã thêm scene vào Build Settings
+    }
+
+    // Hàm thoát game
+    public void QuitGame()
+    {
+        Application.Quit(); // Thoát ứng dụng
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Dừng play mode trong editor
+#endif
+    }
+}
