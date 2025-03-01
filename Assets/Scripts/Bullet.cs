e47d4ad2890c8d72663e424f6e30756f4b10713f
TuanAnhso17 <kid12326@gmail.com>
2025-03-01 21:02:43 +0700

    bb

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        // Kiểm tra xem viên đạn có trúng Enemy không
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // Hủy đối tượng Enemy
+            Destroy(collision.gameObject);
+
+            // Hủy viên đạn
+            Destroy(gameObject);
+        }
+    }
+}
